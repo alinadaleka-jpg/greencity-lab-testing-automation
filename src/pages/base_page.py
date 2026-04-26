d274bbbc8ae494b03feb147e7e4829e4d5b98d36
Mango <alinadaleka@gmail.com>
2026-04-26 15:07:54 +0300

    Update base_page.py

---
+++ src/pages/base_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
+
+    def find(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def click(self, locator):
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        self.driver.execute_script("arguments[0].click();", element)
+
+    def find_all(self, locator):
+        return self.wait.until(EC.presence_of_all_elements_located(locator))
+
+    def safe_click(self, locator):
+        element = self.wait.until(lambda d: d.find_element(*locator))
+        self.driver.execute_script("arguments[0].click();", element)
